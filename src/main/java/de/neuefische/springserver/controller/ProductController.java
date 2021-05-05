c7529bcf640d6968d8ecc112aca6539c8c8bad45
rebekkaschaub <re.schaub@web.de>
2021-05-05 15:18:26 +0200

    Add ProductController.getProductList

--- src/main/java/de/neuefische/springserver/controller/ProductController.java
+++ src/main/java/de/neuefische/springserver/controller/ProductController.java
@@ -1,4 +1,30 @@
 package de.neuefische.springserver.controller;
 
+import de.neuefische.springserver.model.Product;
+import de.neuefische.springserver.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("product")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getProductList(){
+        return productService.getProductList();
+    }
+
+
+
 }
