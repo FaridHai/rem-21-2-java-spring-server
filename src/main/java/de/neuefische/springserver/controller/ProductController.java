c11089601903f26399b0dabdd170a6b220805c4c
rebekkaschaub <re.schaub@web.de>
2021-05-05 15:30:58 +0200

    Add ProductController

--- src/main/java/de/neuefische/springserver/controller/ProductController.java
+++ src/main/java/de/neuefische/springserver/controller/ProductController.java
@@ -3,13 +3,16 @@ package de.neuefische.springserver.controller;
 import de.neuefische.springserver.model.Product;
 import de.neuefische.springserver.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("product")
 public class ProductController {
 
@@ -23,6 +26,31 @@ public class ProductController {
     @GetMapping
     public List<Product> getProductList(){
         return productService.getProductList();
+    }
+
+    @PostMapping
+    public Product addProduct(@Valid @RequestBody Product product){
+        Optional<Product> optionalProduct = productService.addProduct(product);
+        if(optionalProduct.isPresent()){
+            return optionalProduct.get();
+        }else{
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Product already exists");
+        }
+    }
+
+    @PutMapping("{id}")
+    public Product updateProduct(@Valid @RequestBody Product product){
+        return productService.updateProduct(product);
+    }
+
+    @GetMapping("{id}")
+    public Product getProductById(@PathVariable String id){
+        Optional<Product> optionalProduct = productService.getProductById(id);
+        if(optionalProduct.isPresent()){
+            return optionalProduct.get();
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product does not exist");
+        }
     }
 
 
