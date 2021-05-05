76160f82a9dc31ba2a1df0bb656d5dfab4a73cd4
rebekkaschaub <re.schaub@web.de>
2021-05-05 15:08:36 +0200

    Add OrderService Tests

--- src/main/java/de/neuefische/springserver/service/OrderService.java
+++ src/main/java/de/neuefische/springserver/service/OrderService.java
@@ -1,4 +1,37 @@
 package de.neuefische.springserver.service;
 
+import de.neuefische.springserver.model.Order;
+import de.neuefische.springserver.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class OrderService {
+
+    private final OrderRepository orderRepo;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepo) {
+        this.orderRepo = orderRepo;
+    }
+
+    public Order addOrder(Order order){
+        return orderRepo.addOrder(order);
+    }
+
+    public void removeOrderById(String id){
+        orderRepo.removeOrderById(id);
+
+    }
+
+    public List<Order> getOrderList(){
+        return orderRepo.getOrderList();
+    }
+
+    public Optional<Order> getOrderById(String id){
+        return orderRepo.getOrderById(id);
+    }
 }
