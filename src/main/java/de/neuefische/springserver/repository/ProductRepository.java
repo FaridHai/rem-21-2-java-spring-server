ab38f7ac4fa151cab7a6b4dbd47cad1e0fae93bc
farid <farid@haiderzada.de>
2021-05-05 11:54:15 +0200

    Added ProductRepository

--- src/main/java/de/neuefische/springserver/repository/ProductRepository.java
+++ src/main/java/de/neuefische/springserver/repository/ProductRepository.java
@@ -1,4 +1,28 @@
 package de.neuefische.springserver.repository;
 
+import de.neuefische.springserver.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
 public class ProductRepository {
+
+    private final Map<String, Product> productRepo = new HashMap<>();
+
+    public List<Product> getProductList(){
+        return List.copyOf(productRepo.values());
+    }
+
+    public Optional<Product> getProductById(String id){
+        return Optional.ofNullable(productRepo.get(id));
+    }
+
+    public Product addProduct(Product product){
+        productRepo.put(product.getId(), product);
+        return product;
+    }
 }
