ef3d16fe405c57b511d5c9ff04d6f7b6dbecc916
ole-na <82842056+ole-na@users.noreply.github.com>
2021-05-06 12:29:24 +0200

    Add ReviewApi, convert api response

--- src/main/java/de/neuefische/springserver/service/ReviewApiService.java
+++ src/main/java/de/neuefische/springserver/service/ReviewApiService.java
@@ -1,22 +1,50 @@
 package de.neuefische.springserver.service;
 
+import de.neuefische.springserver.model.Product;
 import de.neuefische.springserver.model.Review;
+import de.neuefische.springserver.model.ReviewApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class ReviewApiService {
     private final String reviewApiUrl = "https://jsonplaceholder.typicode.com/comments";
 
     private final RestTemplate restTemplate = new RestTemplate();
+    private final ProductService productService;
 
-     public List<Review> listReviews() {
-         ResponseEntity<Review[]> responseEntity = restTemplate.getForEntity(reviewApiUrl, Review[].class);
-         List<Review> reviews = Arrays.asList(responseEntity.getBody());
-         return reviews;
-     }
+    @Autowired
+    public ReviewApiService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public List<ReviewApi> listApiReviews() {
+        ResponseEntity<ReviewApi[]> responseEntity = restTemplate.getForEntity(reviewApiUrl, ReviewApi[].class);
+        if (responseEntity.getBody() != null) {
+            List<ReviewApi> reviews = Arrays.asList(responseEntity.getBody());
+            return reviews;
+        }
+        return List.of();
+    }
+
+    public List<Review> listReviews() {
+        List<Review> reviews = new ArrayList<>();
+        for (ReviewApi reviewApi : listApiReviews()) {
+            Review review = new Review();
+            review.setId(reviewApi.getId());
+            review.setContent(reviewApi.getBody());
+            review.setEmail(reviewApi.getEmail());
+            review.setTitle(reviewApi.getName());
+            Optional<Product> productOptional = productService.getProductById(reviewApi.getPostId());
+            if (productOptional.isPresent()) {
+                review.setProduct(productOptional.get());
+                reviews.add(review);
+            }
+        }
+        return reviews;
+    }
 }
