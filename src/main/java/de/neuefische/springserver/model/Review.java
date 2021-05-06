d0008b8b831d5d0716b6c93ceea5d8303851520f
rebekkaschaub <re.schaub@web.de>
2021-05-06 11:30:59 +0200

    Adjust Review Class

--- src/main/java/de/neuefische/springserver/model/Review.java
+++ src/main/java/de/neuefische/springserver/model/Review.java
@@ -7,37 +7,22 @@ import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Review {
-    @NotBlank(message = "enter a review id")
-    @JsonProperty("id")
-    private String reviewId;
-    @NotBlank(message = "enter a product id")
-    @JsonProperty("postId")
-    private String productId;
     private Product product;
+
+    @NotBlank(message = "enter a review id")
+    private String id;
+
     @NotBlank(message = "enter a title")
     @JsonProperty("name")
     private String title;
+
     private String email;
+
     @NotBlank(message = "write your review!!!!")
     @Size(min=10, message = "Your review is not long enough!")
     @JsonProperty("body")
     private String content;
 
-    public String getReviewId() {
-        return reviewId;
-    }
-
-    public void setReviewId(String reviewId) {
-        this.reviewId = reviewId;
-    }
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
 
     public Product getProduct() {
         return product;
@@ -45,6 +30,14 @@ public class Review {
 
     public void setProduct(Product product) {
         this.product = product;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getTitle() {
@@ -76,20 +69,19 @@ public class Review {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Review review = (Review) o;
-        return Objects.equals(reviewId, review.reviewId) && Objects.equals(productId, review.productId) && Objects.equals(product, review.product) && Objects.equals(title, review.title) && Objects.equals(email, review.email) && Objects.equals(content, review.content);
+        return Objects.equals(product, review.product) && Objects.equals(id, review.id) && Objects.equals(title, review.title) && Objects.equals(email, review.email) && Objects.equals(content, review.content);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(reviewId, productId, product, title, email, content);
+        return Objects.hash(product, id, title, email, content);
     }
 
     @Override
     public String toString() {
         return "Review{" +
-                "reviewId='" + reviewId + '\'' +
-                ", productId='" + productId + '\'' +
-                ", product=" + product +
+                "product=" + product +
+                ", id='" + id + '\'' +
                 ", title='" + title + '\'' +
                 ", email='" + email + '\'' +
                 ", content='" + content + '\'' +
